9dd6be5ff542b89fd7a307d9a0e0309e8f67ed67
Thomas Belián <72987757+bt909@users.noreply.github.com>
2023-08-21 22:13:31 +0200

    feat: add more verbose endpoints (#20)
    Signed-off-by: Thomas Belian <thomas.belian@bt909.de>

--- cmd/imap-mailstat-exporter/main.go
+++ cmd/imap-mailstat-exporter/main.go
@@ -30,6 +30,25 @@ func main() {
 	mux := http.NewServeMux()
 	promHandler := promhttp.HandlerFor(reg, promhttp.HandlerOpts{})
 	mux.Handle("/metrics", promHandler)
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, ` <!DOCTYPE html>
+		<html>
+		<body>
+		Hello, this is imap-mailstat-exporter, for metrics check
+		<a href="/metrics">metrics endpoint</a>
+        for code check <a href="https://github.com/bt909/imap-mailstat-exporter" target="_blank"> the github repository</a>
+		and for healthchecks you can use <a href="/healthz"> a healthz endpoint</>.
+		</body>
+		</html>`)
+	})
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, ` <!DOCTYPE html>
+		<html>
+		<body>
+		imap-mailstat-exporter is healthy
+		</body>
+		</html>`)
+	})
 
 	port := 8081
 	if err := http.ListenAndServe(fmt.Sprintf(":%d", port), mux); err != nil {
