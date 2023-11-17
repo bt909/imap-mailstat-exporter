eb3564579a4d85f1ff993af00f9cac0cf82e6224
Thomas Belian <thomas.belian@bt909.de>
2023-11-17 18:47:32 +0000

    BREAKING CHANGE: remove old metrics
    Signed-off-by: Thomas Belian <thomas.belian@bt909.de>

--- internal/valuecollect/valuecollector.go
+++ internal/valuecollect/valuecollector.go
@@ -20,40 +20,28 @@ import (
 )
 
 type imapStatsCollector struct {
-	up                   *prometheus.Desc
-	allMails             *prometheus.Desc
-	unseenMails          *prometheus.Desc
-	mailboxQuotaUsed     *prometheus.Desc
-	mailboxQuotaAvail    *prometheus.Desc
-	levelQuotaUsed       *prometheus.Desc
-	levelQuotaAvail      *prometheus.Desc
-	storageQuotaUsed     *prometheus.Desc
-	storageQuotaAvail    *prometheus.Desc
-	messageQuotaUsed     *prometheus.Desc
-	messageQuotaAvail    *prometheus.Desc
-	oldestUnseen         *prometheus.Desc
-	fetchDuration        *prometheus.Desc
-	allMailsOld          *prometheus.Desc
-	unseenMailsOld       *prometheus.Desc
-	mailboxQuotaUsedOld  *prometheus.Desc
-	mailboxQuotaAvailOld *prometheus.Desc
-	levelQuotaUsedOld    *prometheus.Desc
-	levelQuotaAvailOld   *prometheus.Desc
-	storageQuotaUsedOld  *prometheus.Desc
-	storageQuotaAvailOld *prometheus.Desc
-	messageQuotaUsedOld  *prometheus.Desc
-	messageQuotaAvailOld *prometheus.Desc
-	oldestUnseenOld      *prometheus.Desc
-	info                 *prometheus.Desc
-	configfile           string
-	logger               log.Logger
-	oldestunseenfeature  bool
-	migrationmode        bool
-	version              string
+	up                  *prometheus.Desc
+	allMails            *prometheus.Desc
+	unseenMails         *prometheus.Desc
+	mailboxQuotaUsed    *prometheus.Desc
+	mailboxQuotaAvail   *prometheus.Desc
+	levelQuotaUsed      *prometheus.Desc
+	levelQuotaAvail     *prometheus.Desc
+	storageQuotaUsed    *prometheus.Desc
+	storageQuotaAvail   *prometheus.Desc
+	messageQuotaUsed    *prometheus.Desc
+	messageQuotaAvail   *prometheus.Desc
+	oldestUnseen        *prometheus.Desc
+	fetchDuration       *prometheus.Desc
+	info                *prometheus.Desc
+	configfile          string
+	logger              log.Logger
+	oldestunseenfeature bool
+	version             string
 }
 
 // provide metric "layout"
-func NewImapStatsCollector(configfile string, logger log.Logger, oldestunseenfeature bool, migrationmode bool, version string) *imapStatsCollector {
+func NewImapStatsCollector(configfile string, logger log.Logger, oldestunseenfeature bool, version string) *imapStatsCollector {
 	return &imapStatsCollector{
 		up:   prometheus.NewDesc("mailstat_up", "Was talking to all accounts imap successfully.", nil, nil),
 		info: prometheus.NewDesc("mailstat_info", "Info metric for imap-mailstat-exporter.", []string{"version"}, nil),
@@ -117,65 +105,9 @@ func NewImapStatsCollector(configfile string, logger log.Logger, oldestunseenfea
 			"Duration for fetching the metrics for the given account",
 			[]string{"mailboxname"}, nil,
 		),
-		allMailsOld: prometheus.NewDesc(
-			prometheus.BuildFQName("imap_mailstat", "mails_all", "quantity"),
-			"The total number of mails in folder.",
-			[]string{"mailboxname", "mailboxfoldername"}, nil,
-		),
-		unseenMailsOld: prometheus.NewDesc(
-			prometheus.BuildFQName("imap_mailstat", "mails_unseen", "quantity"),
-			"The total number of unseen mails in folder.",
-			[]string{"mailboxname", "mailboxfoldername"}, nil,
-		),
-		mailboxQuotaUsedOld: prometheus.NewDesc(
-			prometheus.BuildFQName("imap_mailstat", "mails_mailboxquotaused", "quantity"),
-			"How many mailboxes are used.",
-			[]string{"mailboxname", "mailboxfoldername"}, nil,
-		),
-		mailboxQuotaAvailOld: prometheus.NewDesc(
-			prometheus.BuildFQName("imap_mailstat", "mails_mailboxquotaavail", "quantity"),
-			"How many mailboxes are available according your quota.",
-			[]string{"mailboxname", "mailboxfoldername"}, nil,
-		),
-		levelQuotaUsedOld: prometheus.NewDesc(
-			prometheus.BuildFQName("imap_mailstat", "mails_levelquotaused", "quantity"),
-			"How many levels are used.",
-			[]string{"mailboxname", "mailboxfoldername"}, nil,
-		),
-		levelQuotaAvailOld: prometheus.NewDesc(
-			prometheus.BuildFQName("imap_mailstat", "mails_levelquotaavail", "quantity"),
-			"How many levels are available according your quota.",
-			[]string{"mailboxname", "mailboxfoldername"}, nil,
-		),
-		storageQuotaUsedOld: prometheus.NewDesc(
-			prometheus.BuildFQName("imap_mailstat", "mails_storagequotaused", "kilobytes"),
-			"How many storage is used.",
-			[]string{"mailboxname", "mailboxfoldername"}, nil,
-		),
-		storageQuotaAvailOld: prometheus.NewDesc(
-			prometheus.BuildFQName("imap_mailstat", "mails_storagequotaavail", "kilobytes"),
-			"How many storage is available according your quota.",
-			[]string{"mailboxname", "mailboxfoldername"}, nil,
-		),
-		messageQuotaUsedOld: prometheus.NewDesc(
-			prometheus.BuildFQName("imap_mailstat", "mails_messagequotaused", "quantity"),
-			"How many messages are used.",
-			[]string{"mailboxname", "mailboxfoldername"}, nil,
-		),
-		messageQuotaAvailOld: prometheus.NewDesc(
-			prometheus.BuildFQName("imap_mailstat", "mails_messagequotaavail", "quantity"),
-			"How many messages available according your quota.",
-			[]string{"mailboxname", "mailboxfoldername"}, nil,
-		),
-		oldestUnseenOld: prometheus.NewDesc(
-			prometheus.BuildFQName("imap_mailstat", "mails_oldestunseen", "timestamp"),
-			"Timestamp in unix format of oldest unseen mail.",
-			[]string{"mailboxname", "mailboxfoldername"}, nil,
-		),
 		configfile:          configfile,
 		logger:              logger,
 		oldestunseenfeature: oldestunseenfeature,
-		migrationmode:       migrationmode,
 		version:             version,
 	}
 
@@ -266,17 +198,6 @@ func (valuecollector *imapStatsCollector) Describe(ch chan<- *prometheus.Desc) {
 	ch <- valuecollector.messageQuotaAvail
 	ch <- valuecollector.oldestUnseen
 	ch <- valuecollector.fetchDuration
-	ch <- valuecollector.allMailsOld
-	ch <- valuecollector.unseenMailsOld
-	ch <- valuecollector.mailboxQuotaUsedOld
-	ch <- valuecollector.mailboxQuotaAvailOld
-	ch <- valuecollector.levelQuotaUsedOld
-	ch <- valuecollector.levelQuotaAvailOld
-	ch <- valuecollector.storageQuotaUsedOld
-	ch <- valuecollector.storageQuotaAvailOld
-	ch <- valuecollector.messageQuotaUsedOld
-	ch <- valuecollector.messageQuotaAvailOld
-	ch <- valuecollector.oldestUnseenOld
 }
 
 // collect values and put them in metrics channel
@@ -348,26 +269,17 @@ func (valuecollector *imapStatsCollector) Collect(ch chan<- prometheus.Metric) {
 			var metricnameSeenInbox []string
 			metricnameSeenInbox = append(metricnameSeenInbox, metricSeenInbox, namespaceSeenInBox)
 			ch <- prometheus.MustNewConstMetric(valuecollector.allMails, prometheus.GaugeValue, float64(countAllmailsInbox), metricnameSeenInbox...)
-			if valuecollector.migrationmode {
-				ch <- prometheus.MustNewConstMetric(valuecollector.allMailsOld, prometheus.GaugeValue, float64(countAllmailsInbox), metricnameSeenInbox...)
-			}
 
 			metricUnseenInbox, namespaceUnseenInbox, countUnseenInbox, _ := countUnseen(c, selectedInbox, valuecollector.logger, config.Accounts[account].Name, valuecollector.oldestunseenfeature)
 			var metricnameUnseenInbox []string
 			metricnameUnseenInbox = append(metricnameUnseenInbox, metricUnseenInbox, namespaceUnseenInbox)
 			ch <- prometheus.MustNewConstMetric(valuecollector.unseenMails, prometheus.GaugeValue, float64(countUnseenInbox), metricnameUnseenInbox...)
-			if valuecollector.migrationmode {
-				ch <- prometheus.MustNewConstMetric(valuecollector.unseenMailsOld, prometheus.GaugeValue, float64(countUnseenInbox), metricnameUnseenInbox...)
-			}
 
 			metricOldestUnseenInbox, namespaceOldestUnseenInbox, _, timestampOldestUnseenInbox := countUnseen(c, selectedInbox, valuecollector.logger, config.Accounts[account].Name, valuecollector.oldestunseenfeature)
 			var metricnameOldestUnseenInbox []string
 			if timestampOldestUnseenInbox > 0 {
 				metricnameOldestUnseenInbox = append(metricnameOldestUnseenInbox, metricOldestUnseenInbox, namespaceOldestUnseenInbox)
 				ch <- prometheus.MustNewConstMetric(valuecollector.oldestUnseen, prometheus.GaugeValue, float64(timestampOldestUnseenInbox), metricnameOldestUnseenInbox...)
-				if valuecollector.migrationmode {
-					ch <- prometheus.MustNewConstMetric(valuecollector.oldestUnseenOld, prometheus.GaugeValue, float64(timestampOldestUnseenInbox), metricnameOldestUnseenInbox...)
-				}
 			}
 
 			qc := quota.NewClient(c)
@@ -380,65 +292,41 @@ func (valuecollector *imapStatsCollector) Collect(ch chan<- prometheus.Metric) {
 				var metricnameMailboxQuotaUsed []string
 				metricnameMailboxQuotaUsed = append(metricnameMailboxQuotaUsed, metricMailboxQuotaUsed, namespaceMailboxQuotaUsed)
 				ch <- prometheus.MustNewConstMetric(valuecollector.mailboxQuotaUsed, prometheus.GaugeValue, float64(countMailboxQuotaUsed["MAILBOX_used"]), metricnameMailboxQuotaUsed...)
-				if valuecollector.migrationmode {
-					ch <- prometheus.MustNewConstMetric(valuecollector.mailboxQuotaUsedOld, prometheus.GaugeValue, float64(countMailboxQuotaUsed["MAILBOX_used"]), metricnameMailboxQuotaUsed...)
-				}
 
 				metricMailboxQuotaAvail, namespaceMailboxQuotaAvail, _, countMailboxQuotaAvail := getMailboxUsed(qc, selectedInbox, valuecollector.logger, config.Accounts[account].Name)
 				var metricnameMailboxQuotaAvail []string
 				metricnameMailboxQuotaAvail = append(metricnameMailboxQuotaAvail, metricMailboxQuotaAvail, namespaceMailboxQuotaAvail)
 				ch <- prometheus.MustNewConstMetric(valuecollector.mailboxQuotaAvail, prometheus.GaugeValue, float64(countMailboxQuotaAvail["MAILBOX_avail"]), metricnameMailboxQuotaAvail...)
-				if valuecollector.migrationmode {
-					ch <- prometheus.MustNewConstMetric(valuecollector.mailboxQuotaAvailOld, prometheus.GaugeValue, float64(countMailboxQuotaAvail["MAILBOX_avail"]), metricnameMailboxQuotaAvail...)
-				}
 
 				metricLevelQuotaUsed, namespaceLevelQuotaUsed, countLevelQuotaUsed, _ := getMailboxUsed(qc, selectedInbox, valuecollector.logger, config.Accounts[account].Name)
 				var metricnameLevelQuotaUsed []string
 				metricnameLevelQuotaUsed = append(metricnameLevelQuotaUsed, metricLevelQuotaUsed, namespaceLevelQuotaUsed)
 				ch <- prometheus.MustNewConstMetric(valuecollector.levelQuotaUsed, prometheus.GaugeValue, float64(countLevelQuotaUsed["LEVEL_used"]), metricnameLevelQuotaUsed...)
-				if valuecollector.migrationmode {
-					ch <- prometheus.MustNewConstMetric(valuecollector.levelQuotaUsedOld, prometheus.GaugeValue, float64(countLevelQuotaUsed["LEVEL_used"]), metricnameLevelQuotaUsed...)
-				}
 
 				metricLevelQuotaAvail, namespaceLevelQuotaAvail, _, countLevelQuotaAvail := getMailboxUsed(qc, selectedInbox, valuecollector.logger, config.Accounts[account].Name)
 				var metricnameLevelQuotaAvail []string
 				metricnameLevelQuotaAvail = append(metricnameLevelQuotaAvail, metricLevelQuotaAvail, namespaceLevelQuotaAvail)
 				ch <- prometheus.MustNewConstMetric(valuecollector.levelQuotaAvail, prometheus.GaugeValue, float64(countLevelQuotaAvail["LEVEL_avail"]), metricnameLevelQuotaAvail...)
-				if valuecollector.migrationmode {
-					ch <- prometheus.MustNewConstMetric(valuecollector.levelQuotaAvailOld, prometheus.GaugeValue, float64(countLevelQuotaAvail["LEVEL_avail"]), metricnameLevelQuotaAvail...)
-				}
 
 				metricStorageQuotaUsed, namespaceStorageQuotaUsed, countStorageQuotaUsed, _ := getMailboxUsed(qc, selectedInbox, valuecollector.logger, config.Accounts[account].Name)
 				var metricnameStorageQuotaUsed []string
 				metricnameStorageQuotaUsed = append(metricnameStorageQuotaUsed, metricStorageQuotaUsed, namespaceStorageQuotaUsed)
 				ch <- prometheus.MustNewConstMetric(valuecollector.storageQuotaUsed, prometheus.GaugeValue, float64(countStorageQuotaUsed["STORAGE_used"]), metricnameStorageQuotaUsed...)
-				if valuecollector.migrationmode {
-					ch <- prometheus.MustNewConstMetric(valuecollector.storageQuotaUsedOld, prometheus.GaugeValue, float64(countStorageQuotaUsed["STORAGE_used"]/1000), metricnameStorageQuotaUsed...)
-				}
 
 				metricStorageQuotaAvail, namespaceStorageQuotaAvail, _, countStorageQuotaAvail := getMailboxUsed(qc, selectedInbox, valuecollector.logger, config.Accounts[account].Name)
 				var metricnameStorageQuotaAvail []string
 				metricnameStorageQuotaAvail = append(metricnameStorageQuotaAvail, metricStorageQuotaAvail, namespaceStorageQuotaAvail)
 				ch <- prometheus.MustNewConstMetric(valuecollector.storageQuotaAvail, prometheus.GaugeValue, float64(countStorageQuotaAvail["STORAGE_avail"]), metricnameStorageQuotaAvail...)
-				if valuecollector.migrationmode {
-					ch <- prometheus.MustNewConstMetric(valuecollector.storageQuotaAvailOld, prometheus.GaugeValue, float64(countStorageQuotaAvail["STORAGE_avail"]/1000), metricnameStorageQuotaAvail...)
-				}
 
 				metricMessageQuotaUsed, namespaceMessageQuotaUsed, countMessageQuotaUsed, _ := getMailboxUsed(qc, selectedInbox, valuecollector.logger, config.Accounts[account].Name)
 				var metricnameMessageQuotaUsed []string
 				metricnameMessageQuotaUsed = append(metricnameMessageQuotaUsed, metricMessageQuotaUsed, namespaceMessageQuotaUsed)
 				ch <- prometheus.MustNewConstMetric(valuecollector.messageQuotaUsed, prometheus.GaugeValue, float64(countMessageQuotaUsed["MESSAGE_used"]), metricnameMessageQuotaUsed...)
-				if valuecollector.migrationmode {
-					ch <- prometheus.MustNewConstMetric(valuecollector.messageQuotaUsedOld, prometheus.GaugeValue, float64(countMessageQuotaUsed["MESSAGE_used"]), metricnameMessageQuotaUsed...)
-				}
 
 				metricMessageQuotaAvail, namespaceMessageQuotaAvail, _, countMessageQuotaAvail := getMailboxUsed(qc, selectedInbox, valuecollector.logger, config.Accounts[account].Name)
 				var metricnameMessageQuotaAvail []string
 				metricnameMessageQuotaAvail = append(metricnameMessageQuotaAvail, metricMessageQuotaAvail, namespaceMessageQuotaAvail)
 				ch <- prometheus.MustNewConstMetric(valuecollector.messageQuotaAvail, prometheus.GaugeValue, float64(countMessageQuotaAvail["MESSAGE_avail"]), metricnameMessageQuotaAvail...)
-				if valuecollector.migrationmode {
-					ch <- prometheus.MustNewConstMetric(valuecollector.messageQuotaAvailOld, prometheus.GaugeValue, float64(countMessageQuotaAvail["MESSAGE_avail"]), metricnameMessageQuotaAvail...)
-				}
 			}
 
 			for _, f := range config.Accounts[account].Folders {
@@ -457,26 +345,17 @@ func (valuecollector *imapStatsCollector) Collect(ch chan<- prometheus.Metric) {
 				var metricnameSeen []string
 				metricnameSeen = append(metricnameSeen, metricSeen, namespaceSeen)
 				ch <- prometheus.MustNewConstMetric(valuecollector.allMails, prometheus.GaugeValue, float64(countAllmails), metricnameSeen...)
-				if valuecollector.migrationmode {
-					ch <- prometheus.MustNewConstMetric(valuecollector.allMailsOld, prometheus.GaugeValue, float64(countAllmails), metricnameSeen...)
-				}
 
 				metricUnseen, namespaceUnseen, countUnseenMails, _ := countUnseen(c, selected, valuecollector.logger, config.Accounts[account].Name, valuecollector.oldestunseenfeature)
 				var metricnameUnseen []string
 				metricnameUnseen = append(metricnameUnseen, metricUnseen, namespaceUnseen)
 				ch <- prometheus.MustNewConstMetric(valuecollector.unseenMails, prometheus.GaugeValue, float64(countUnseenMails), metricnameUnseen...)
-				if valuecollector.migrationmode {
-					ch <- prometheus.MustNewConstMetric(valuecollector.unseenMailsOld, prometheus.GaugeValue, float64(countUnseenMails), metricnameUnseen...)
-				}
 
 				metricOldestUnseen, namespaceOldestUnseen, _, timestampOldestUnseen := countUnseen(c, selected, valuecollector.logger, config.Accounts[account].Name, valuecollector.oldestunseenfeature)
 				if timestampOldestUnseen > 0 {
 					var metricnameOldestUnseen []string
 					metricnameOldestUnseen = append(metricnameOldestUnseen, metricOldestUnseen, namespaceOldestUnseen)
 					ch <- prometheus.MustNewConstMetric(valuecollector.oldestUnseen, prometheus.GaugeValue, float64(timestampOldestUnseen), metricnameOldestUnseen...)
-					if valuecollector.migrationmode {
-						ch <- prometheus.MustNewConstMetric(valuecollector.oldestUnseenOld, prometheus.GaugeValue, float64(timestampOldestUnseen), metricnameOldestUnseen...)
-					}
 				}
 			}
 			ch <- prometheus.MustNewConstMetric(valuecollector.fetchDuration, prometheus.GaugeValue, float64(time.Since(start).Seconds()), strings.ReplaceAll(strings.ReplaceAll(config.Accounts[account].Name, ".", "_"), " ", "_"))
