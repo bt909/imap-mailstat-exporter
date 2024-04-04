921a19129bd60e16dfbf1231aab59259012548ee
Thomas Belian <thomas.belian@bt909.de>
2024-04-04 06:15:09 +0000

    chore: bump version
    Signed-off-by: Thomas Belian <thomas.belian@bt909.de>

--- cmd/imap-mailstat-exporter/main.go
+++ cmd/imap-mailstat-exporter/main.go
@@ -20,7 +20,7 @@ import (
 
 var (
 	name                = "imap-mailstat-exporter"
-	Version             = "0.3.0-beta"
+	Version             = "0.3.0"
 	configfile          *string
 	oldestunseenfeature *bool
 	mailboxpassword     *string
