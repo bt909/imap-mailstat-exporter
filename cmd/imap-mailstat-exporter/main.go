ff2d6ffb490dc117530e73244ec513908c826fe4
Thomas Belian <thomas.belian@bt909.de>
2024-11-20 19:40:48 +0000

    chore: adjust changelog
    Signed-off-by: Thomas Belian <thomas.belian@bt909.de>

--- cmd/imap-mailstat-exporter/main.go
+++ cmd/imap-mailstat-exporter/main.go
@@ -20,7 +20,7 @@ import (
 
 var (
 	name                = "imap-mailstat-exporter"
-	Version             = "0.6.0"
+	Version             = "0.7.0-alpha"
 	configfile          *string
 	oldestunseenfeature *bool
 	mailboxpassword     *string
