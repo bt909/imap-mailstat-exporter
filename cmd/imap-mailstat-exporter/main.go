32b41c944ce689f3cfefd715b95bccb3d24683cc
Thomas Belian <thomas.belian@bt909.de>
2024-12-22 21:29:31 +0000

    chore: update version
    Signed-off-by: Thomas Belian <thomas.belian@bt909.de>

--- cmd/imap-mailstat-exporter/main.go
+++ cmd/imap-mailstat-exporter/main.go
@@ -20,7 +20,7 @@ import (
 
 var (
 	name                = "imap-mailstat-exporter"
-	Version             = "0.6.1"
+	Version             = "0.6.2"
 	configfile          *string
 	oldestunseenfeature *bool
 	mailboxpassword     *string
